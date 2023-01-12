92fd7e369082dc57890116fd4f9f5fa9a5d9f19e
Your Name <you@example.com>
2023-01-11 20:26:02 -0800

    adding remaining stub routes

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 router.get('/',(req,res)=>{
-    res.send('GET /places')
+    res.send('GET /places <h1> This is the PLACES Page </h1>')
 })
 
 module.exports = router
