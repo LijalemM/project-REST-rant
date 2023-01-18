4c039fcc03eff75b7db1b5bf3fd35b8186c2b06d
Your Name <you@example.com>
2023-01-18 11:02:32 -0800

    adding style to REST-Rant

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,19 @@ function home () {
     return (
         <Def>
             <main>
-                <h1> HOME SWEET HOME </h1>
+                <h1> HOME </h1>
+                <p>Short Paragraph on Restaurant
+A restaurant is a place where people visit to eat and drink the food being prepared on the premises and pays for the same. The food is served at the table to have a comfortable visit for your meals. The restaurant offers a menu with various options for your meal, to choose from.
+
+Restaurants provide you with different cuisines of food to satisfy your hunger.
+The food preparation and presentation gives you a happy feel during your dull
+The ambiance that a restaurant provides you with gives one all the more reason to visit to cherish their time.
+The restaurant is a life savior when one does not feel like cooking or wants to eat something else.
+You get to explore places when you plan to visit a restaurant.
+The fast-food restaurant provides your meals on the go to save your time at affordable prices. </p>
+                <a href='/places'>
+                    <button className='btn-primary'> Places Page </button>
+                </a>
             </main>
         </Def>
     )
